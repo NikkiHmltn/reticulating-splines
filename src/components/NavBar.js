bcb6f156874359835444e191c46545969c050f1f
NikkiHmltn <nikkihmltn1@yahoo.com>
2022-04-25 18:00:18 -0700

    set up nav

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,23 +1,30 @@
 import {NavLink} from 'react-router-dom'
+import '../styles/NavBar.css'
+import simsBanner from '../images/simsbannerplaceholder.png'
 
 function NavBar() {
   return (
-    <nav>
-        <ul>
-            <li>
-                <NavLink to="/">Home</NavLink>
-            </li>
-            <li>
-                <NavLink to="/about">About</NavLink>
-            </li>
-            <li>
-                <NavLink to="/donate">Donate</NavLink>
-            </li>
-            <li>
-                <NavLink to="/contact">Feedback</NavLink>
-            </li>
-        </ul>
-    </nav>
+    <>
+      <div className='banner-container'>
+        <img src={simsBanner} alt="sims blog banner"/>
+      </div>
+        <nav className="nav-navbar">
+            <ul className="nav-ul">
+                <li>
+                    <NavLink to="/" className={({isActive}) => (isActive ? "active-navlink" : "navlink")}>Home</NavLink>
+                </li>
+                <li>
+                    <NavLink to="/about" className={({isActive}) => (isActive ? "active-navlink" : "navlink")}>About</NavLink>
+                </li>
+                <li>
+                    <NavLink to="/donate" className={({isActive}) => (isActive ? "active-navlink" : "navlink")}>Donate</NavLink>
+                </li>
+                <li>
+                    <NavLink to="/contact" className={({isActive}) => (isActive ? "active-navlink" : "navlink")}>Feedback</NavLink>
+                </li>
+            </ul>
+        </nav>
+    </>
   );
 }
 
