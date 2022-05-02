f65791e372ca1986b0957c28f4384fa3bb986e10
NikkiHmltn <nikkihmltn1@yahoo.com>
2022-05-02 14:39:53 -0700

    icon display test

--- src/components/StartHouseGen.js
+++ src/components/StartHouseGen.js
@@ -4,29 +4,84 @@ import '../styles/StartHouse.css'
 import M from  'materialize-css'
 
 const StartHouseGen = () => {
-    const [packs, setPacks] = useState([])
+    // const [allPacks, setAllPacks] = useState([])
+    const [expansionPacks, setExpansionPacks] = useState([])
+    const [gamePacks, setGamePacks] = useState([])
+    const [kitPacks, setKitPacks] = useState([])
+    const [stuffPacks, setStuffPacks] = useState([])
+
+    let elems = document.querySelectorAll('.tooltipped');
+        M.Tooltip.init(elems, {});
+
     useEffect(()=> {
         simsApi.get("/sims-packs")
         .then((res) => {
-            setPacks(res.data.data)
+            const allPacks = res.data.data
+            for(let i=0; i < allPacks.length; i++) {
+                let packType = allPacks[i].type
+                if (packType === 'Expansion') {
+                    setExpansionPacks(expansionPacks => [...expansionPacks, allPacks[i]])
+                }else if (packType === 'Game') {
+                    setGamePacks(gamePacks => [...gamePacks, allPacks[i]])
+                } else if (packType === 'Kit') {
+                    setKitPacks(kitPacks => [...kitPacks, allPacks[i]])
+                } else if (packType === 'Stuff') {
+                    setStuffPacks(stuffPacks => [...stuffPacks, allPacks[i]])
+                    console.log("set stuff packs hello")
+                } 
+            }  
         })
-
-        let elems = document.querySelectorAll('.tooltipped');
-        M.Tooltip.init(elems, {});
-
+        // .then(() => {
+        //     for(let i=0; i < allPacks.length; i++) {
+        //         let packType = allPacks[i].type
+        //         if (packType === 'Expansion') {
+        //             setExpansionPacks(expansionPacks => [...expansionPacks, allPacks[i]])
+        //         }else if (packType === 'Game') {
+        //             setGamePacks(gamePacks => [...gamePacks, allPacks[i]])
+        //         } else if (packType === 'Kit') {
+        //             setKitPacks(kitPacks => [...kitPacks, allPacks[i]])
+        //         } else if (packType === 'Stuff') {
+        //             setStuffPacks(stuffPacks => [...stuffPacks, allPacks[i]])
+        //             console.log("set stuff packs hello")
+        //         } 
+        //     }   
+        // })
     }, [])
-    console.log(packs[0])
+
+    // const separatePacks = () => {
+    //     console.log(allPacks)
+        
+    // }
+
+
+
+    
+    const renderDivs = (packTypeArr) => {
+        return packTypeArr.map((pack) => {
+            return (
+                <div className={pack.type + ' pack-icon'}>
+                    <a className="tooltipped pack-icon" data-position="top" data-tooltip={pack.name}>
+                        <img src={pack.icon}/>
+                    </a>
+                </div>
+            )
+        })
+    }
+
     return(
         <div className='pack-container'>
-            {packs.map((packInfo => {
-                return (
-                    <div className={packInfo.type + ' pack-icon'}>
-                        <a className="tooltipped pack-icon" data-position="top" data-tooltip={packInfo.name}>
-                            <img src={packInfo.icon}/>
-                        </a>
-                    </div>
-                )
-            }))}
+            <div className='expansion-container'>
+                {renderDivs(expansionPacks)}
+            </div>
+            <div className='game-container'>
+                {renderDivs(gamePacks)}
+            </div>
+            <div className='stuff-container'>
+                {renderDivs(stuffPacks)}
+            </div>
+            <div className='kit-container'>
+                {renderDivs(kitPacks)}
+            </div>
         </div>
     )
 }
