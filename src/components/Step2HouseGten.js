be77b9995e87492f1267a32f405edd2919066b47
NikkiHmltn <nikkihmltn1@yahoo.com>
2022-05-10 21:28:57 -0700

    step2 fully functional and added more state

--- src/components/Step2HouseGten.js
+++ src/components/Step2HouseGten.js
@@ -2,12 +2,19 @@ import { useEffect } from 'react';
 import M from 'materialize-css'
 import '../styles/TwoHouse.css'
 
-const Step2HouseGen = () => {
+const Step2HouseGen = ({changeBuildSettings}) => {
 
 
-    const handleSubmit = (e) => {
+    const handleChange = (e) => {
         // change this to onChange => handle change
         // get some conditionals to set state
+        let name = e.target.name
+        let value = e.target.value
+        if (name == "randomize" || name == "colorpalette"){
+            changeBuildSettings(name, e.target.checked)
+            return;
+        }
+        changeBuildSettings(name, value)
     }
 
     useEffect(()=> {
@@ -22,56 +29,54 @@ const Step2HouseGen = () => {
                 <p>Cat ipsum dolor sit amet, kitty scratches couch bad kitty yet thinking about you i'm joking it's food always food. Scratch me there, elevator butt nyan nyan goes the cat, scraaaaape scraaaape goes the walls when the cat murders them with its claws, scratch leg; meow for can opener to feed me but knock over christmas tree purr while eating.</p>
             </div>
             <div className="divider"></div>
-            <form>
-                <div className="options-container">
-                    <div>
-                        <p class="range-field">
-                            Lot Traits
-                            <input type="range" id="test5" min="0" max="3" name='traits' />
-                        </p>
-                        <p class="range-field">
-                            Lot Challenges
-                            <input type="range" id="test5" min="0" max="5" name='challenges' />
-                        </p>
-                        <p class="range-field">
-                            Number of Sims
-                            <input type="range" id="test5" min="1" max="8" name='numSims' />
-                        </p>
-                        <p class="range-field">
-                            Number of Rooms
-                            <input type="range" id="test5" min="0" max="8" name='numRooms' />
-                        </p>
-                        <p class="range-field">
-                            Budget
-                            <input type="range" id="test5" min="10000" max="100000" name='budget' />
-                        </p>
-                        <div className='checkbox-step2-container'>
-                            <label>
-                                <input type="checkbox" value="colorpalette" name="colorpalette" />
-                                <span>Color Palette</span>
-                            </label>
-                            <label>
-                                <input type="checkbox" value="randomize" name="randomize" />
-                                <span>Randomize All</span>
-                            </label>
-                        </div>
+            <div className="options-container">
+                <div>
+                    <p class="range-field">
+                        Lot Traits
+                        <input type="range" id="test5" min="0" max="3" name='traits' onChange={handleChange} />
+                    </p>
+                    <p class="range-field">
+                        Lot Challenges
+                        <input type="range" id="test5" min="0" max="5" name='challenges' onChange={handleChange}/>
+                    </p>
+                    <p class="range-field">
+                        Number of Sims
+                        <input type="range" id="test5" min="1" max="8" name='numSims' onChange={handleChange}/>
+                    </p>
+                    <p class="range-field">
+                        Number of Rooms
+                        <input type="range" id="test5" min="0" max="8" name='numRooms' onChange={handleChange}/>
+                    </p>
+                    <p class="range-field">
+                        Budget
+                        <input type="range" id="test5" min="10000" max="100000" name='budget' onChange={handleChange}/>
+                    </p>
+                    <div className='checkbox-step2-container'>
+                        <label>
+                            <input type="checkbox"  name="colorpalette" onChange={handleChange}/>
+                            <span>Color Palette</span>
+                        </label>
+                        <label>
+                            <input type="checkbox"  name="randomize" onChange={handleChange}/>
+                            <span>Randomize All</span>
+                        </label>
                     </div>
                 </div>
-                <div className='radio-container'>
-                    <label>
-                        <input name="step1" type="radio" disabled="disabled" />
-                        <span></span>
-                    </label>
-                    <label>
-                        <input name="step2" type="radio" checked/>
-                        <span></span>
-                    </label>
-                </div>
-                <div className='button-container center-align'>
-                    <button className="btn waves-effect waves-light" type="submit" onSubmit={handleSubmit}>Generate House
-                    </button>
-                </div>  
-            </form>
+            </div>
+            <div className='radio-container'>
+                <label>
+                    <input name="step1" type="radio" disabled="disabled" />
+                    <span></span>
+                </label>
+                <label>
+                    <input name="step2" type="radio" checked/>
+                    <span></span>
+                </label>
+            </div>
+            <div className='button-container center-align'>
+                <button className="btn waves-effect waves-light" type="submit">Generate House
+                </button>
+            </div>  
         </div>
     )
 }
