37c3de440ba63c5a1cdfd99daeeabb4fb72f6ad0
NikkiHmltn <nikkihmltn1@yahoo.com>
2022-05-10 21:01:57 -0700

    fixed navbar dropdown finally

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -20,9 +20,14 @@ function NavBar() {
       <div className='banner-container'>
         <img src={simsBanner} alt="sims blog banner"/>
       </div>
+
       <ul id='gen-drop' className='dropdown-content'>
-          <li><NavLink to="/generators/sim" className={({isActive}) => (isActive ? "active-navlink" : "navlink")}>Sim Generator</NavLink></li>
-          <li><NavLink to="/generators/home" className={({isActive}) => (isActive ? "active-navlink" : "navlink")}>House Generator</NavLink></li>
+            <li><NavLink to="/generators/sim" className={({isActive}) => (isActive ? "active-navlink" : "navlink")}>Sim Generator</NavLink></li>
+            <li><NavLink to="/generators/home" className={({isActive}) => (isActive ? "active-navlink" : "navlink")}>House Generator</NavLink></li>
+      </ul>
+      <ul id='gen-drop-2' className='dropdown-content'>
+            <li><NavLink to="/generators/sim" className={({isActive}) => (isActive ? "active-navlink" : "navlink")}>Sim Generator</NavLink></li>
+            <li><NavLink to="/generators/home" className={({isActive}) => (isActive ? "active-navlink" : "navlink")}>House Generator</NavLink></li>
       </ul>
 
       <nav>
@@ -36,7 +41,7 @@ function NavBar() {
           <NavLink to="/posts" className={({isActive}) => (isActive ? "active-navlink" : "navlink")}>Blog</NavLink>
         </li>
         {/* <!-- Dropdown Trigger --> */}
-        <li><a className="dropdown-trigger" href="#!" data-target="gen-drop">Generators<i className="material-icons right">arrow_drop_down</i></a></li>
+        <li><a className="dropdown-trigger" href="#!" data-target="gen-drop" data-activates="gen-drop">Generators<i className="material-icons right">arrow_drop_down</i></a></li>
         <li>                    
           <NavLink to="/donate" className={({isActive}) => (isActive ? "active-navlink" : "navlink")}>Donate</NavLink>
         </li>
@@ -47,25 +52,26 @@ function NavBar() {
         </div>
       </nav>
 
+      
    
-    <ul className="sidenav" id="slide-out">
-    <li>
-          <NavLink to="/" className={({isActive}) => (isActive ? "active-navlink" : "navlink")}>Home</NavLink>
-        </li>
-        <li>
-          <NavLink to="/posts" className={({isActive}) => (isActive ? "active-navlink" : "navlink")}>Blog</NavLink>
-        </li>
-        <li>
-          {/* <!-- Dropdown Trigger --> */}
-        <a className='dropdown-trigger' href='#' data-target='gen-drop'>Generators</a>
-        </li>
-        <li>                    
-          <NavLink to="/donate" className={({isActive}) => (isActive ? "active-navlink" : "navlink")}>Donate</NavLink>
-        </li>
-        <li>
-          <NavLink to="/contact" className={({isActive}) => (isActive ? "active-navlink" : "navlink")}>Feedback</NavLink>
-        </li>
-    </ul>
+      <ul className="sidenav" id="slide-out">
+      <li>
+            <NavLink to="/" className={({isActive}) => (isActive ? "active-navlink" : "navlink")}>Home</NavLink>
+          </li>
+          <li>
+            <NavLink to="/posts" className={({isActive}) => (isActive ? "active-navlink" : "navlink")}>Blog</NavLink>
+          </li>
+          <li>
+            
+          <a className='dropdown-trigger' href='#' data-target='gen-drop-2'>Generators</a>
+          </li>
+          <li>                    
+            <NavLink to="/donate" className={({isActive}) => (isActive ? "active-navlink" : "navlink")}>Donate</NavLink>
+          </li>
+          <li>
+            <NavLink to="/contact" className={({isActive}) => (isActive ? "active-navlink" : "navlink")}>Feedback</NavLink>
+          </li>
+      </ul>
     </div>
   );
 }
