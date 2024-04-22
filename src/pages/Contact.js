4fb31b699595419a3998588e2434601ed96b3876
Nicole Hamilton <andromeda@Nicoles-Air-2.lan>
2024-04-21 20:20:27 -0700

    set up contact details

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,12 +1,21 @@
-import {Box, Heading, Flex} from 'gestalt'
+import {Box, Heading, Flex, Text} from 'gestalt'
+import { Link } from 'react-router-dom';
 
 export default function Contact() {
     return(
-        <Flex height="75vh" alignContent='center' justifyContent='center'>
-            <Box alignContent='center'>
+        <Flex height="75vh" justifyContent='center'>
+            <Box margin={10}>
                 <Heading size="500">
-                    Oops! Nothing here. Please check back later for this feature!
+                    Contact Details
                 </Heading>
+                <hr></hr>
+                <Text>Please send any bugs, issues, requests, kind words to:</Text>
+                <br></br>
+                <Text><Link to="#"
+                onClick={(e) => {
+                    window.location.href = "mailto:snikkidoodle21@gmail.com";
+                    e.preventDefault()
+                }}>snikkidoodle21@gmail.com</Link></Text>
             </Box>
         </Flex>
     )
